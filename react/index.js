be7ff893a184ad52d646553637e098c27355782e
shwetapund <pundshweta2020@gmail.com>
2023-09-24 22:37:36 -0700

    includes function practice

---
+++ react/index.js
@@ -0,0 +1,27 @@
+const phonebook = [
+    {
+        name: 'shweta',
+        mobile: 8208793074
+    },
+    {
+        name: 'shital',
+        mobile: 9309382528
+    },
+    {
+        name: 'rahul',
+        mobile: 9999978200
+    }
+]
+
+// const filteredArray = [];
+
+// phonebook.forEach((ContactDetail) => {
+//     const {name, mobile} = ContactDetail;
+
+//     if(name.includes('s')){
+//         filteredArray.push(ContactDetail)
+//     }
+// })
+
+// console.log(filteredArray);
+
