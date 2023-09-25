2b8ccfc3a94c1449d416a5840b57afb331ff1f62
shwetapund <pundshweta2020@gmail.com>
2023-09-24 23:15:35 -0700

    filter function practice

--- react/index.js
+++ react/index.js
@@ -25,3 +25,12 @@ const phonebook = [
 
 // console.log(filteredArray);
 
+const searchTerm = '25';
+
+const filteredArray = phonebook.filter( (ContactDetail) => {
+    const name = ContactDetail.name.toLowerCase();
+    const mobile = ContactDetail.mobile.toString();
+    return (name.includes(searchTerm) || mobile.includes(searchTerm));
+
+  })
+  console.log(filteredArray);
