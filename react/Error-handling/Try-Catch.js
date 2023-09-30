236d6d77dca58ddd8127c738c9b165c12ec75d22
shwetapund <pundshweta2020@gmail.com>
2023-09-29 18:37:46 -0700

    practice try catch

--- react/Error-handling/Try-Catch.js
+++ react/Error-handling/Try-Catch.js
@@ -1,18 +1,26 @@
-console.log("hii i am shweta");
-console.log("hii i am shital");
+// console.log("hii i am shweta");
+// console.log("hii i am shital");
 
-try{ 
-    throw new Error('you have an error')  //throwing error
+// try{ 
+//     throw new Error('you have an error')  //throwing error
+// }
+// catch(e){
+
+//     console.log(e.stack);
+// } //try-catch statement use
+// console.log("hii shweta"); 
+
+function SayHello() {
+    return 'Hello'
+}
+let msg = null ;
+try{
+    
+msg = SayHello()
 }
 catch(e){
-
-    console.log(e.stack);
-} //try-catch statement use
-console.log("hii shweta"); 
-
-
-
-
-
+    console.log(e)
+}
+console.log(msg);
 
 
