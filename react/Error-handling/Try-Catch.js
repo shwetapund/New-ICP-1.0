65832a33429ce2aea43dc6cfc7b5f0fd530b4f35
shwetapund <pundshweta2020@gmail.com>
2023-09-29 18:27:02 -0700

    try-catch statement use practice

---
+++ react/Error-handling/Try-Catch.js
@@ -0,0 +1,18 @@
+console.log("hii i am shweta");
+console.log("hii i am shital");
+
+try{ 
+    throw new Error('you have an error')  //throwing error
+}
+catch(e){
+
+    console.log(e.stack);
+} //try-catch statement use
+console.log("hii shweta"); 
+
+
+
+
+
+
+
