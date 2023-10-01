209d334cf5709b2502757c21b3dcc4ac95e95f51
shwetapund <pundshweta2020@gmail.com>
2023-09-30 21:23:54 -0700

    async-await-practice

--- react/Async-Await-promises/Async-await.js
+++ react/Async-Await-promises/Async-await.js
@@ -1,19 +1,41 @@
 async function runforLongTime() {
- for(let i=0; i<1000000000; i++){}
-//  throw new Error('something went wrong')
-return "Done"
+
+    for (let i = 0; i < 1000000000; i++) { }
+    throw new Error('you have an error');
+    return "function finished"
 
 }
+//function fullfill
+// const p = runforLongTime();
+// console.log(p);
 
+// p.then((result) =>{
+//      console.log('promise resolve');
+//      console.log(result);  
 
-const p = runforLongTime();
-console.log(p);
-p.then((result) =>{
-    console.log('promise resolve')
-    console.log(result);
-}).catch((e) =>{
-    console.log("error");
-    console.log(e);
-});
+// }).catch((e) =>{     
+//     console.log('error caught');
+//     console.log(e); 
+// })
+(async function(){
+    try {
+        const result = await runforLongTime();
+        console.log(result);
+    }
+    catch (e) {
+        console.log(e.message);
+    }
 
-// runforShortTime();
+})();
+
+async function apiCall() {
+    return{
+        type: 'API_CALL'
+    }
+}
+const triggerAPICall = async() =>{
+    const response = await apiCall();
+    console.log(response);
+}
+const response = await apiCall();
+console.log(response);
